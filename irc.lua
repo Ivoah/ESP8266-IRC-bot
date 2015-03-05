91f5491744e51d3a03d05aa23d809dbd8db240bb
Ivoah <ivoahivoah@gmail.com>
2015-03-05 11:40:01 -0500

    Add commands

--- irc.lua
+++ irc.lua
@@ -46,7 +46,13 @@ function receive(irc, text)
         end
     elseif text:find("QUIT") then
         --:SpeedEvil!~quassel@tor/regular/SpeedEvil QUIT :Read error: Connection reset by peer
-
+        local _, i = text:find("QUIT")
+        local chnl_msg = text:sub(i + 2)
+        local i = chnl_msg:find(":")
+        local v = text:find("!")
+        if v then
+            print(text:sub(2, v - 1) .. " has left IRC " .. "(" .. chnl_msg:sub(i + 1) .. ")\r\n")
+        end
     elseif text:find("JOIN") then
         --:Ivoah!~Ivoah@p-74-209-20-44.dsl1.rtr.chat.fpma.frpt.net JOIN #esp8266
         local _, i = text:find("JOIN")
@@ -66,32 +72,57 @@ end
 function handle_message(usr, chnl, msg)
     if chnl == nick then chnl = usr end
     print("Message from " .. usr .. " in " .. chnl .. ": " .. msg)
-    if msg:find(nick) then
+    if chnl == usr or msg:find(nick) then
         send_message(chnl, "Hi there " .. usr)
     elseif msg:find("~") == 1 then
-
+        --~google esp8266 irc bot
+        local cmd = split(msg)
+        handle_command(chnl, cmd[1]:sub(2), {select(2, unpack(cmd))})
     end
 end
 
 function handle_command(chnl, cmd, args)
+    if cmd == "google" then
+        send_message(chnl, "http://google.com/search&q=" .. url_encode(table.concat(args, " ")))
+    elseif cmd == "d" then
+        if #args ~= 0 and #args ~= 1 then
+            send_message(chnl, "Usage: ~d [sides] (default: 6)")
+        end
+        local n = math.floor(tonumber(args[1]) or 6)
+        send_message("d" .. n .. " roll: " .. math.random(n))
 
+    end
 end
 
 function send_message(chnl, msg)
     irc:send("PRIVMSG " .. chnl .. " :" .. msg .. "\r\n")
 end
 
-function split(str, sep)
+function split(inputstr, sep)
     if sep == nil then
         sep = "%s"
     end
-    local t={} ; i=1
+    local t = {}
+    local i = 1
     for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
         t[i] = str
         i = i + 1
     end
     return t
 end
+
+function url_encode(str)
+  if str then
+    str = string.gsub (str, "\n", "\r\n")
+    str = string.gsub (str, "([^%w %-%_%.%~])", function (c)
+        return string.format ("%%%02X", string.byte(c))
+    end)
+    str = string.gsub (str, " ", "+")
+  end
+  return str
+end
+
+math.randomseed(adc.read(0))
 
 irc = net.createConnection(net.TCP, 0)
 irc:on("connection", connection)
